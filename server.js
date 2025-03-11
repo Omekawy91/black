c61b86739f6d0eebb04889a9b4b1e85909026f80
Omekawy91 <156941679+Omekawy91@users.noreply.github.com>
2025-03-11 07:11:23 +0200

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
